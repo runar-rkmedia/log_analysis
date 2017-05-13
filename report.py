3c08694a699d0404cf352357586ab5d5d553ddd4
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-05-14 00:23:07 +0200

    Fix orderings

--- report.py
+++ report.py
@@ -16,18 +16,18 @@ def top_articles(articles_returned=3):
     connection = connect(CONNECTION)
     cursor = connection.cursor(cursor_factory=extras.DictCursor)
     cursor.execute("""
-                   SELECT *
-                   FROM
-                       (SELECT articles.*,
-                               num AS views
-                        FROM
-                            (SELECT *
-                             FROM top_articles_in_log LIMIT (%s)) subsub,
-                             articles
-                        WHERE path = '/article/' || slug
-                        ORDER BY views DESC) sub,
-                        authors
-                   WHERE authors.id=author ;
+                    SELECT *
+                    FROM
+                        (SELECT articles.*,
+                                num AS views
+                         FROM
+                             (SELECT *
+                              FROM top_articles_in_log LIMIT (%s)) subsub,
+                              articles
+                         WHERE path = '/article/' || slug ) sub,
+                         authors
+                    WHERE authors.id=author
+                    ORDER BY views DESC;
                                    """, (articles_returned, ))
     articles = cursor.fetchall()
 
@@ -35,9 +35,11 @@ def top_articles(articles_returned=3):
     connection.close()
     return articles
 
+
 def topAuthors(authors_returned=3):
     """Return a list of most popular authors by page-views."""
     pass
+
 
 def daysOfErrors(percentile=1):
     """Return a list of days where there were more than <percentile> errors."""
