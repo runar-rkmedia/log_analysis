4cf77463b5517381de2fa54d09e9d5a6a035eb4f
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-05-14 00:00:36 +0200

    import only needed modules

--- report.py
+++ report.py
@@ -5,16 +5,16 @@ Part of a udacity assignment.
 Code by Runar Kristoffersen.
 """
 
-import psycopg2
-from psycopg2 import extras
+# import psycopg2
+from psycopg2 import extras, connect
 
 CONNECTION = "dbname=news"
 
 
 def top_articles(articles_returned=3):
     """Return a list of the most popular articles."""
-    connection = psycopg2.connect(CONNECTION)
-    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    connection = connect(CONNECTION)
+    cursor = connection.cursor(cursor_factory=extras.DictCursor)
     cursor.execute("""
                    SELECT articles.*,
                           num AS VIEWS
